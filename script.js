5c542e4c55383d2f411485fdbe283ee2d0f3f502
DeathGame20250213 <hutaomeru3@googlemail.com>
2025-02-09 12:14:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkPassword() {
+    const correctPassword = "1234"; // 設定するパスワード
+    const inputPassword = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (inputPassword === correctPassword) {
+        window.location.href = "success.html"; // 成功ページへリダイレクト
+    } else {
+        message.textContent = "パスワードが間違っています。";
+    }
+}
